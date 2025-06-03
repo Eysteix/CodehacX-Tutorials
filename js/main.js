cd51bb6f1f0007e29b72b1cf1d41bc84826ce53e
Wonder <vision060607@gmail.com>
2025-06-03 16:17:16 +0000

    new search query manager

--- js/main.js
+++ js/main.js
@@ -1,23 +1,66 @@
-const search = async (value) => {
+const search = async (value, e) => {
   const main = document.querySelector(".movie-boxes");
+  const searchResults = document.querySelector(".search-results");
+  const resultsBox = document.querySelector(".results-box");
 
-  const movies = await fetch("http://127.0.0.1:5500/js/anime.json", {
-    cache: "default",
-  });
-  const data = await movies.json();
+  const [animes, cartoons,chinese,donghuas,series] = await Promise.all(
+    [
+      fetch("http://127.0.0.1:5500/js/anime.json", {
+        cache: "default",
+      }),
+      fetch("http://127.0.0.1:5500/js/cartoons.json", {
+        cache: "default",
+      }),
+       fetch("http://127.0.0.1:5500/js/chinese_anime.json", {
+        cache: "default",
+      }),
+       fetch("http://127.0.0.1:5500/js/donghua.json", {
+        cache: "default",
+      }),
+       fetch("http://127.0.0.1:5500/js/series.json", {
+        cache: "default",
+      }),
+    ]
+  );
+
+  const anime = await animes.json();
+  const cartoon = await cartoons.json();
+  const chinese_anime =  await chinese.json();
+  const  donghua  = await donghuas.json();
+  const seriesData =  await  series.json()
+
+  // console.log(animes);
+
+  const data = [...anime.animes,...cartoon.cartoons,...chinese_anime.chinese_animes,...donghua.donghuas,...seriesData.seriesData]
+
+
+
+   main.innerHTML = data
+    .map(
+      (item) =>
+        `<div class="movie-box">${item.title ? item.title : item.name}</div>`
+    )
+    .join("");
+
 
   if (value && value.length != 0) {
     const newData = data.filter((item) => {
-      return item.name.toLowerCase().includes(String(value.toLowerCase()));
+      return (item.name ? item.name : item.title)
+        .toLowerCase()
+        .includes(String(value.toLowerCase()));
     });
-    return (main.innerHTML = newData
-      .map((item) => `<div class="movie-box">${item.name}</div>`)
+    const tenEL = newData.slice(0,10);
+    searchResults.classList.add("active");
+    return ( resultsBox.innerHTML = tenEL
+      .map(
+        (item) =>
+          `<div class="search-item">${item.title ? item.title : item.name}</div>`
+      )
       .join(""));
   }
+   searchResults.classList.remove("active");
 
-  main.innerHTML = data
-    .map((item) => `<div class="movie-box">${item.name}</div>`)
-    .join("");
+
 };
 
 search();
